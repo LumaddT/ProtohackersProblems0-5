471bb64ef6fd877ee7ed66af8b5f9ef08af379bd
LumaddT <93663941+LumaddT@users.noreply.github.com>
2025-02-20 21:18:15 +0200

    Implement problem 04, Unusual Database Program, first draft

--- src/main/java/proto/hackers/Main.java
+++ src/main/java/proto/hackers/Main.java
@@ -2,7 +2,7 @@ package proto.hackers;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import proto.hackers.problem03_BudgetChat.BudgetChat;
+import proto.hackers.problem04_UnusualDatabaseProgram.UnusualDatabaseProgram;
 
 @SuppressWarnings("CommentedOutCode")
 public class Main {
@@ -16,7 +16,8 @@ public class Main {
 //        new Thread(() -> SmokeTest.run(10_000)).start();
 //        new Thread(() -> PrimeTime.run(10_001)).start();
 //        new Thread(() -> MeansToAnEnd.run(10_002)).start();
-        new Thread(() -> BudgetChat.run(10_003)).start();
+//        new Thread(() -> BudgetChat.run(10_003)).start();
+        new Thread(() -> UnusualDatabaseProgram.run(10_004)).start();
     }
 
     private static void shutdownRoutine() {
@@ -25,7 +26,8 @@ public class Main {
 //        SmokeTest.stop();
 //        PrimeTime.stop();
 //        MeansToAnEnd.stop();
-        BudgetChat.stop();
+//        BudgetChat.stop();
+        UnusualDatabaseProgram.stop();
 
         logger.info("Have a nice day!.");
     }
---
+++ src/main/java/proto/hackers/problem04_UnusualDatabaseProgram/UnusualDatabaseProgram.java
@@ -0,0 +1,92 @@
+package proto.hackers.problem04_UnusualDatabaseProgram;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UnusualDatabaseProgram {
+    private static final Logger logger = LogManager.getLogger();
+
+    private static final int TIMEOUT = 1_000;
+
+    private static volatile boolean Running = false;
+
+    private static final String VERSION = "My Key-Value Store 1.0";
+    private static final int MAX_LENGTH = 1_000;
+    private static final Map<String, String> DATABASE = new HashMap<>();
+
+    public static void run(int port) {
+        if (Running) {
+            logger.warn("Attempted to run, but this is already running.");
+            return;
+        }
+
+        Running = true;
+
+        try (DatagramSocket serverSocket = new DatagramSocket(port)) {
+            logger.info("Started on port {}.", port);
+
+            serverSocket.setSoTimeout(TIMEOUT);
+
+            while (Running) {
+                try {
+                    checkSocket(serverSocket);
+                } catch (SocketTimeoutException e) {
+                    logger.trace("Socket timed out (timeout: {}) in thread {}.", TIMEOUT, Thread.currentThread().toString());
+                }
+            }
+        } catch (IOException e) {
+            logger.fatal("An IO exception was thrown by the DatagramSocket. No attempt will be made to reopen the socket.\n{}\n{}", e.getMessage(), e.getStackTrace());
+        }
+    }
+
+    private static void checkSocket(DatagramSocket socket) throws IOException {
+        byte[] buf = new byte[MAX_LENGTH];
+
+        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+
+        String clientMessage = new String(packet.getData(), 0, packet.getLength()).trim();
+
+        if (clientMessage.contains("=")) {
+            String[] splitClientMessage = clientMessage.split("=", 2);
+            logger.debug("Putting \"{}\" in the database.", clientMessage);
+            DATABASE.put(splitClientMessage[0], splitClientMessage[1]);
+        } else {
+            String value;
+            if (clientMessage.equals("version")) {
+                value = VERSION;
+            } else {
+                value = DATABASE.getOrDefault(clientMessage, "");
+            }
+
+            String serverMessageString = clientMessage + '=' + value;
+
+            logger.debug("Retrieving \"{}\" from the database.", serverMessageString);
+            byte[] serverMessage = serverMessageString.getBytes();
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+
+            packet = new DatagramPacket(serverMessage, serverMessage.length, address, port);
+
+            socket.send(packet);
+        }
+    }
+
+
+    public static void stop() {
+        if (Running) {
+            logger.info("Stopped.");
+        } else {
+            logger.warn("Attempted to stop, but this is already stopped.");
+        }
+        Running = false;
+    }
+}
